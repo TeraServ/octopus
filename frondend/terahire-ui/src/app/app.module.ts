149bcd223f8778fc78cd4e27b47546be0599c779
alanrs2022 <alan.sathyaseelan@teranet.ca>
2023-01-27 15:59:29 +0530

    all-comp-update

--- frondend/terahire-ui/src/app/app.module.ts
+++ frondend/terahire-ui/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './home/dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { RecruitmentComponent } from './home/recruitment/recruitment.component';
+import { AdminComponent } from './home/admin/admin.component';
+import { CalendarComponent } from './home/calendar/calendar.component';
+import { SettingsComponent } from './home/settings/settings.component';
+import { ProgressTableComponent } from './home/dashboard/progress-table/progress-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    RecruitmentComponent,
+    AdminComponent,
+    CalendarComponent,
+    SettingsComponent,
+    ProgressTableComponent
   ],
   imports: [
     BrowserModule,
